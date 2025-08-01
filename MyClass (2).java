861c2d32d0cb3beb3030742f4b3653182628030e
lanalessa <laninhateo@gmail.com>
2025-08-01 17:02:21 -0300

    Exercício Java

---
+++ MyClass (2).java
@@ -0,0 +1,38 @@
+public class MyClass {
+  public static void main(String args[]) {
+   
+   //1 - Cálculo para empresa 
+     double instalacao = 300.00;
+     double limpeza = 150.00;
+     double manutencao = 499.99;
+     double pagar = instalacao + limpeza + manutencao;
+     System.out.println ("O cliente deverá pagar: " + pagar );
+     
+     //2 - Cálculo de salário
+     double valor = 35.00;
+     double hora = 8;
+     double salario = valor * hora;
+     System.out.println ("O salário do Joaquim é " + salario + " reais por dia");
+     
+     //3 - Caixas de bombons
+     int bombom = 27;
+     int caixas = 16;
+     int totalDoces = bombom * caixas;
+      System.out.println ("Pamela ficou com " + totalDoces + " bombons");
+     
+     //4 - Garrafas de água
+     int garrafas = 300;
+     int caixa = 20;
+     int usaram = garrafas / caixa;
+     System.out.println ("Eles usaram " + usaram + " caixas");
+     
+     //5 - Cálculo para camisetas 
+     
+     
+     //6 - Dobro e Metade
+
+
+
+   
+  }
+}
